8d6698d823a3adf3ed070b74eb0155441978090a
saki <manicon22@gmail.com>
2016-06-26 05:11:20 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
@@ -6,10 +10,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "welcom to my app"
+      redirect_to @user
     else
       render "new"
     end
   end
+  
+  
+  
   
   private 
   def user_params
