30722708ef164c52c9bb6e431818e008a645202f
saki <manicon22@gmail.com>
2016-07-09 10:38:25 +0000

    フォロー一覧実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts
   resources :relationships, only: [:create, :destroy]
+  
+  resources :users do
+    member do
+      get 'followings'
+      get 'followers'
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
